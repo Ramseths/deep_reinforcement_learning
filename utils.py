007b3e8ed8cc263719422820c4515b44c4bea76b
Ramseths <jesus.ramseths@gmail.com>
2023-09-24 01:06:17 -0600

    feat: add params and load image

---
+++ utils.py
@@ -0,0 +1,4 @@
+target_urls = { 
+               "Art" : "http://www.indianruminations.com/wp-content/uploads/what-is-modern-art-definition-2.jpg",
+               "Abstract" : "https://scx2.b-cdn.net/gfx/news/2020/abstractart.jpg"
+               }
