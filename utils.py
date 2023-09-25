dff2ff65501e06ca1351b30cee1727848b5d06fb
Ramseths <jesus.ramseths@gmail.com>
2023-09-25 10:31:59 -0600

    feat: show resuts

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
 target_urls = { 
                "Art" : "http://www.indianruminations.com/wp-content/uploads/what-is-modern-art-definition-2.jpg",
-               "Abstract" : "https://scx2.b-cdn.net/gfx/news/2020/abstractart.jpg"
                }
